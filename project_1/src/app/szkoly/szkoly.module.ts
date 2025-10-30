fa7a8601b7a3195d0f4aaa6f730a3533479d9647
Ilaria Ziarek <10197483u@zs1piaseczno.local>
2025-10-30 14:58:46 +0100

    Dodano! Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.

--- project_1/src/app/szkoly/szkoly.module.ts
+++ project_1/src/app/szkoly/szkoly.module.ts
@@ -9,4 +9,8 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class SzkolyModule { }
+export class SzkolyModule {
+	dodajSzkole(arg0: string, arg1: string, arg2: string, arg3: string) {
+		throw new Error('Method not implemented.');
+	}
+}
---
+++ sciezki.bat
@@ -0,0 +1,6 @@
+rem SET-ALIAS -NAME NG -VALUE C:\USERS\10197483U\APPData\roaming\npm\ng.cmd
+PATH=%PATH%;C:\USERS\10197483U\APPData\roaming\npm
+PATH=%PATH%;z:\git\bin
+bash
+
+
